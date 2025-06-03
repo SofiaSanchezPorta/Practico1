de543e50299d7e8f073ad21941445e69170f227c
sofisp <sofisapo927@gmail.com>
2025-06-02 22:17:56 -0300

    Implementación con javaScript de textarea dinámico que aparece cuando se selecciona la opción otros, y desaparece ante el cambio a otra opción

--- js/contacto.js
+++ js/contacto.js
@@ -1,2 +1,11 @@
-const comoConoce = document.getElementById(comoConoce);
+const seleccion = document.getElementById("seleccion");  
+const otro = document.getElementById("otro");
 
+comoConoce.addEventListener("change", function(){
+     const opcion = this.value;
+     if(opcion == "3"){
+        otro.classList.remove("otros");
+    }else{
+        otro.classList.add("otros");
+    }
+})
