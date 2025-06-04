a3beb97181744af6ebb385b0b5d942766e6888d7
sofisp <sofisapo927@gmail.com>
2025-06-03 21:38:05 -0300

    Lógica implementada en JS para que al hacer click en el envío del formulario éste desaparezca, y aparezca un saludo de agradecimiento. Aún hay que revisar detalles

--- js/contacto.js
+++ js/contacto.js
@@ -1,5 +1,11 @@
-const seleccion = document.getElementById("seleccion");  
+//const comoConoce = document.getElementById("comoConoce");  
+//ASÍ FUNCIONA, PERO NO ENTIENDO PORQUÉ 
+
 const otro = document.getElementById("otro");
+const enviar = document.getElementById("enviar");
+const formulario = document.getElementsByClassName("formulario");
+const agradecimiento = document.getElementById("gracias");
+console.log(agradecimiento);
 
 comoConoce.addEventListener("change", function(){
      const opcion = this.value;
@@ -7,5 +13,21 @@ comoConoce.addEventListener("change", function(){
         otro.classList.remove("otros");
     }else{
         otro.classList.add("otros");
+    }
+})
+
+enviar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    if(comoConoce.value != ""){ //COMPLETAR CON LÓGICA PARA QUE MAIL Y NOMBRE NO QUEDEN VACÍOS
+        for(let i = 0; i < formulario.length; i++){
+        formulario[i].classList.add("otros");
+        }
+
+        agradecimiento.classList.remove("agradecimiento");
+        //agradecimiento.classList.add("finalizado");
+        //REVISAR CÓMO DARLE ESTILO (MÁS LUGAR) AL MENSAJE DE AGRADECIMIENTO
+
+        //PODRÍA USARSE UN AVISO DE QUE EL FORMULARIO ESTÁ INCOMPLETO
     }
 })
